8b5fdf38d1371033d94dc9eb80a758f740707eab
lanyixiao <44657943+lanyixiao@users.noreply.github.com>
2019-05-17 11:52:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.querySelectorAll("input[type=checkbox]").forEach(element => {
+  
+  const checked = localStorage.getItem(element.name);
+  
+  if (checked == "true") {
+    element.checked = true;
+  }
 
+  element.addEventListener("change", (event) => {
+    localStorage.setItem(event.target.name, event.target.checked);
+  });
+});
